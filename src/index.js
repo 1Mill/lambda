143aee179d2e3df2bbb74ad5a05fdc2213f5d452
1Mill <erik.ekberg@outlook.com>
2021-08-06 10:08:50 -0700

    Release first implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('SOmething')
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
+
+const invoke = async ({
+	// Invocation
+	functionName,
+	invocationType = 'RequestResponse',
+	payload = null,
+	// Credentials
+	accessKeyId = (process && process.env && process.env.LAMBDA_AWS_ACCESS_KEY_ID),
+	region = (process && process.env && process.env.LAMBDA_AWS_REGION),
+	secretAccessKey = (process && process.env && process.env.LAMBDA_AWS_SECRET_ACCESS_KEY),
+	// Optional for development purposes (e.g. localstack)
+	endpoint = (process && process.env && process.env.LAMBDA_AWS_ENDPOINT),
+}) => {
+	try {
+		const aws = new LambdaClient({
+			credentials: {
+				accessKeyId: accessKeyId,
+				secretAccessKey: secretAccessKey,
+			},
+			endpoint,
+			region,
+		})
+		const command = new InvokeCommand({
+			FunctionName: functionName,
+			InvocationType: invocationType,
+			Payload: payload,
+		})
+		const response = await aws.send(command)
+		const data = JSON.parse(Buffer.from(response.Payload).toString())
+		return data
+	} catch (err) {
+		return err
+	}
+}
+
+module.exports = { invoke }
